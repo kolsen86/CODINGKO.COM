54257f4c292b1c9a345becece35b242235ec404f
Kenneth Olsen <kolsen86@yahoo.com>
2018-07-08 10:20:45 -0400

    added pagination for tutorials

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,24 @@ class PagesController < ApplicationController
     def tutorials
     end
 
+    def inttutorials
+    end
+
+    def booktutorials
+    end
+
+    def videotutorials
+    end
+
+    def mooctutorials
+    end
+
+    def paidtutorialsandpath
+    end
+
+    def referanceandquestions
+    end
+
     def tools
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
   get 'grind', to: 'pages#grind'
   get 'tutorials', to: 'pages#tutorials'
   get 'tools', to: 'pages#tools'
+  get 'inttutorials', to: 'pages#inttutorials'
+  get 'booktutorials', to: 'pages#booktutorials'
+  get 'videotutorials', to: 'pages#videotutorials'
+  get 'mooctutorials', to: 'pages#mooctutorials'
+  get 'paidtutorialsandpath', to: 'pages#paidtutorialsandpath'
+  get 'referanceandquestions', to: 'pages#referanceandquestions'
   resources :contacts, only: :create
   get 'contact-me', to: 'contacts#new', as: 'new_contact'
 end
