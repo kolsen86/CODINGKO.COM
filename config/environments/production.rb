1e5393e9e95b23df5d087fce0dc7734c2c37f757
Kenneth Olsen <kolsen86@yahoo.com>
2018-09-22 07:58:38 -0400

    Change active storage service

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-   config.action_controller.asset_host = 'https://dash.cloudflare.com/89c616ae2038b5b1fb8122f43db3ac78/codingko.com'
+   #config.action_controller.asset_host = ''
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
