cccca461b09445b2d1e3b1e7b1f39f65aa756c19
kolsen86 <kolsen86@yahoo.com>
2018-10-23 13:07:51 -0400

    test

--- db/schema.rb
+++ db/schema.rb
@@ -41,11 +41,4 @@ ActiveRecord::Schema.define(version: 2018_06_07_231019) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
